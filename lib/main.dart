ba4b3f2f692de19519000afaa591f5b16950a298
Yovieaji13 <71000479+Yovieaji13@users.noreply.github.com>
2021-03-09 15:26:19 +0700

    progress2

--- lib/main.dart
+++ lib/main.dart
@@ -33,12 +33,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final inputController = TextEditingController();
+  final inputan = TextEditingController();
   List<String> listViewItem = List<String>();
   String _newValue1 = "Watt";
   String _newValue2 = "Megawatt";
   var listItem1 = ["Watt", "Megawatt", "Kilowatt"];
   var listItem2 = ["Watt", "Megawatt", "Kilowatt"];
+  double _hasil = 0;
+  double _input = 0;
 
   void perhitunganPower() {
     setState(() {
@@ -53,32 +55,75 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-
-      body: Container(
-            margin: EdgeInsets.all(8),
-            child: Column(
-              children: [
-
-                DropdownButton<String>(
-                  items: listItem1.map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  value: _newValue1,
-                  onChanged: (String changeValue) {
-                    setState(() {
-                      _newValue1 = changeValue;
-                    });
-                  },
-                ),
     
-                TextFormField(
+      body: ListView(
+        children: <Widget>[
+
+          Column(
+          children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [ 
+                Stack(
+                  children: [
+                      Container(
+                        height: 10,
+                        )  
+                ],),
+
+                  Text("Dari : "),
+                    DropdownButton<String>(
+                    items: listItem1.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    value: _newValue1,
+                    onChanged: (String changeValue) {
+                      setState(() {
+                        _newValue1 = changeValue;
+                      });
+                    },
+                  ),
+
+                  Text("      Ke : "),
+                  DropdownButton<String>(
+                    items: listItem2.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    value: _newValue2,
+                    onChanged: (String changeValue) {
+                      setState(() {
+                        _newValue2 = changeValue;
+                      });
+                    },
+                  ),
+                ],
+              ),
+
+              Stack(
+               children: [
+                 
+                  Container(
+                   margin: EdgeInsets.only(top: 20),
+                   padding: EdgeInsets.only(left: 10),
+                    alignment: Alignment.centerLeft,
+                      // padding: EdgeInsets.only(left: 20.0, bottom: 15.0),
+                      child: Text(
+                         "Inputkan Nominal : ", style: TextStyle(fontSize: 14, color: Colors.black),
+                       ),
+                     )  
+                ],),
+
+               TextFormField(
                   maxLength: 5,
-                  controller: inputController,
+                  controller: inputan,
                   decoration: InputDecoration(
-                    hintText: "Inputkan Nominal Power",
+                    hintText: "",
                   ),
                   inputFormatters: [
                     FilteringTextInputFormatter.allow(RegExp(r'[0-9+\,]'))
@@ -87,36 +132,57 @@ class _MyHomePageState extends State<MyHomePage> {
                       decimal: true, signed: false),
                 ),
 
-                DropdownButton<String>(
-                  items: listItem2.map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  value: _newValue2,
-                  onChanged: (String changeValue) {
-                    setState(() {
-                      _newValue2 = changeValue;
-                    });
-                  },
-                ),
-               
-               RaisedButton(
-                  color: Colors.yellow,
-                  onPressed: perhitunganPower,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Convert Power',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ],
-                   )
+                Stack(
+                  children: [
+                      Container(
+                        alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            "Result : ", style: TextStyle(fontSize: 14, color: Colors.black),
+                          ),
+                        )  
+                ],),
+
+               TextFormField(
+                  controller: inputan,
+                  decoration: InputDecoration(
+                    hintText: "",
+                  ),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9+\,]'))
+                  ],
+                  keyboardType: TextInputType.numberWithOptions(
+                      decimal: true, signed: false),
+                ),    
+
+                Stack(
+                  children: [
+                      Container(
+                        height: 10,
+                        )  
+                ],),
+                
+                SizedBox(
+                  width: 200,
+                  child:RaisedButton(
+                    color: Colors.yellow,
+                    padding: EdgeInsets.all(10),
+                    onPressed: perhitunganPower,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Convert Power',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    )
+                  ),
                 ),
               ],
-            ),
+           ),
+
+        ],
       ),
     );
   }
